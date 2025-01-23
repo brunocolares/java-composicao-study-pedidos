7753f70a183a2204b174cebe53d989f29975c775
bruno <brunocolaresdev@gmail.com>
2025-01-23 18:11:51 -0300

    feat: implementação do program e utilização do StringBuilder

--- src/application/Program.java
+++ src/application/Program.java
@@ -7,6 +7,10 @@ import java.util.Locale;
 import java.util.Scanner;
 
 import entities.Client;
+import entities.Order;
+import entities.OrderItem;
+import entities.Product;
+import entities.enums.OrderStatus;
 
 public class Program {
 	public static void main(String[] args) throws ParseException {
@@ -21,12 +25,16 @@ public class Program {
 		String email = sc.next();
 		System.out.print("Birth date (DD/MM/YYYY): ");
 		Date birthDate = sdf.parse(sc.next());
+		System.out.println("Enter order data:");
+		System.out.print("Status: ");
+		OrderStatus orderStatus = OrderStatus.valueOf(sc.next());
 		
 		Client client = new Client(name, email, birthDate);
+		Order order = new Order(new Date(), orderStatus, client);
 		
 		System.out.print("How many items to this order? ");
 		int n = sc.nextInt();
-		
+		sc.nextLine();
 		for(int i = 1; i <= n; i++) {
 			System.out.println("Enter #" + i + " item data: ");
 			System.out.print("Product name: ");
@@ -35,8 +43,13 @@ public class Program {
 			Double productPrice = sc.nextDouble();
 			System.out.println("Quantity: ");
 			Integer productQuantity = sc.nextInt();
-						
+			sc.nextLine();
+			Product product = new Product(productName, productPrice);
+			OrderItem orderItem = new OrderItem(productQuantity, productPrice, product);
+			order.addItem(orderItem);
 		}
+		System.out.println("");
+		System.out.println(order.toString());
 		
 	}
 }
--- src/entities/Order.java
+++ src/entities/Order.java
@@ -1,5 +1,6 @@
 package entities;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -7,6 +8,8 @@ import java.util.List;
 import entities.enums.OrderStatus;
 
 public class Order {
+	
+	private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
 	
 	private Date moment;
 	private OrderStatus status;
@@ -63,5 +66,21 @@ public class Order {
 			total += item.subTotal();
 		}
 		return total;
+	}
+	
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		sb.append("ORDER SUMMARY\n");
+		sb.append("Order moment: " + sdf.format(getMoment()) + "\n");
+		sb.append("Order status: "+ getStatus().name() + "\n");
+		sb.append(String.format("Client: %s (%s) - %s%n", getClient().getName(), sdf.format(getClient().getBirthDate()).substring(0, 10), getClient().getEmail()));
+		sb.append("Order items:" +"\n");
+		for(OrderItem orderItem : items) {
+			sb.append(String.format("%s, $%.2f, Quantity: %d, Subtotal: $%.2f%n", orderItem.getProduct().getName(), 
+					orderItem.getPrice(), orderItem.getQuantity(), orderItem.subTotal()));
+		}
+		sb.append("Total price: $"+total());
+		
+		return sb.toString();
 	}
 }
