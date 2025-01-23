44f3db4bb575555a85014e74d7af79444dc4586c
bruno <brunocolaresdev@gmail.com>
2025-01-22 23:04:09 -0300

    first commit

---
+++ src/application/Program.java
@@ -0,0 +1,42 @@
+package application;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Client;
+
+public class Program {
+	public static void main(String[] args) throws ParseException {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		
+		System.out.println("Enter cliente data: ");
+		System.out.print("Name: ");
+		String name = sc.nextLine();
+		System.out.print("Email: ");
+		String email = sc.next();
+		System.out.print("Birth date (DD/MM/YYYY): ");
+		Date birthDate = sdf.parse(sc.next());
+		
+		Client client = new Client(name, email, birthDate);
+		
+		System.out.print("How many items to this order? ");
+		int n = sc.nextInt();
+		
+		for(int i = 1; i <= n; i++) {
+			System.out.println("Enter #" + i + " item data: ");
+			System.out.print("Product name: ");
+			String productName = sc.nextLine();
+			System.out.print("Product price: ");
+			Double productPrice = sc.nextDouble();
+			System.out.println("Quantity: ");
+			Integer productQuantity = sc.nextInt();
+						
+		}
+		
+	}
+}
